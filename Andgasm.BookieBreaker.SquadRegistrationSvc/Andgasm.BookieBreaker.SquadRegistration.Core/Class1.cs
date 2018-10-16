c45f4dcba7bad7b671451754637a87e1dc0e0525
Danny Brown <machinagod@googlemail.com>
2018-10-16 18:45:00 +0100

    initial soln wireframe

---
+++ Andgasm.BookieBreaker.SquadRegistrationSvc/Andgasm.BookieBreaker.SquadRegistration.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Andgasm.BookieBreaker.SquadRegistration.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Andgasm.BookieBreaker.SquadRegistrationSvc/Andgasm.BookieBreaker.SquadRegistration.Extractor.Svc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Andgasm.BookieBreaker.SquadRegistration.Extractor.Svc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
